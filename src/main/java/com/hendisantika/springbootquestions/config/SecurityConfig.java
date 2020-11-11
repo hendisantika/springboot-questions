56ead1eb7223c77bcf39bf74f1e9670e80d555c8
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:36:06 +0700

    #18 - Add SecurityConfig

---
+++ src/main/java/com/hendisantika/springbootquestions/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootquestions.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-questions
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.35
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
