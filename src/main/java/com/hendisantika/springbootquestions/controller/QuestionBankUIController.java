6c0858e0f909b6d9d777e10539f5b9a31a1cbde4
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:42:45 +0700

    #30 - Add addCategory route in QuestionBankUIController

--- src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
+++ src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by IntelliJ IDEA.
@@ -42,4 +43,11 @@ public class QuestionBankUIController {
         model.addAttribute("questions", questionBankService.fetchAllQuestionsAndAnswers());
         return "questions";
     }
+
+    @PostMapping("/category/add")
+    public String addCategory(Model model) {
+        model.addAttribute("category", new Category());
+        return "addCategory";
+    }
+
 }
