76a031599b1c46e36e3f91b4c832ea1545ecece8
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:24:03 +0700

    #5 - Add QuestionAnswers Entity

---
+++ src/main/java/com/hendisantika/springbootquestions/entity/QuestionAnswers.java
@@ -0,0 +1,60 @@
+package com.hendisantika.springbootquestions.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-questions
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.23
+ */
+@Entity
+@Table(name = "question_answers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionAnswers implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_id", insertable = false, nullable = false)
+    private Integer questionId;
+
+    @OneToOne
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @CreationTimestamp
+    @Column(name = "created_ts", nullable = false)
+    private LocalDateTime createdTs;
+
+    @UpdateTimestamp
+    @Column(name = "last_updated_ts", nullable = false)
+    private LocalDateTime lastUpdatedTs;
+
+}
