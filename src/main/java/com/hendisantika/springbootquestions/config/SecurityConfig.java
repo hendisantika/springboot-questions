b9390828f0a85ce0eafd6dd4f0c35a0099d78b3f
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:36:40 +0700

    #19 - Add userDetailsService & passwordEncoder bean SecurityConfig

--- src/main/java/com/hendisantika/springbootquestions/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootquestions/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.hendisantika.springbootquestions.config;
 
+import com.hendisantika.springbootquestions.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +19,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
