390c4d19af5fc746d354436c3f48d9187c4c912b
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:42:09 +0700

    #28 - Add showAllCategories route in QuestionBankUIController

--- src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
+++ src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootquestions.controller;
 import com.hendisantika.springbootquestions.service.QuestionBankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -28,5 +29,11 @@ public class QuestionBankUIController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/category")
+    public String showAllCategories(Model model) {
+        model.addAttribute("categories", questionBankService.fetchAllCategories());
+        return "category";
     }
 }
