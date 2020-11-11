8feb399740824a142307edac2fdb4722298377e0
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:34:31 +0700

    #16 - Add deleteCategory, fetchCategory & fetchQuestionAnswer service in QuestionBankService

--- src/main/java/com/hendisantika/springbootquestions/service/QuestionBankService.java
+++ src/main/java/com/hendisantika/springbootquestions/service/QuestionBankService.java
@@ -51,4 +51,19 @@ public class QuestionBankService implements IQuestionBankService {
     public void deleteQuestion(String questionId) {
         questionAnswersRepository.deleteById(Integer.valueOf(questionId));
     }
+
+    @Override
+    public void deleteCategory(String categoryId) {
+        categoryRepository.deleteById(Integer.valueOf(categoryId));
+    }
+
+    @Override
+    public Category fetchCategory(String categoryId) {
+        return categoryRepository.findById(Integer.valueOf(categoryId)).orElse(null);
+    }
+
+    @Override
+    public QuestionAnswers fetchQuestionAnswer(String questionId) {
+        return questionAnswersRepository.findById(Integer.valueOf(questionId)).orElse(null);
+    }
 }
