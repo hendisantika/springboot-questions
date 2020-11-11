13bba39ea1c2a792599125a3fc2f2b603ab45d57
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:08:43 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootquestions/SpringbootQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
