dc8a48d35fd78637e984c1106ecad4c48bc58445
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-12 05:51:05 +0700

    #38 - Add oauthUserInfo route in QuestionBankUIController

--- src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
+++ src/main/java/com/hendisantika/springbootquestions/controller/QuestionBankUIController.java
@@ -4,12 +4,19 @@ import com.hendisantika.springbootquestions.entity.Category;
 import com.hendisantika.springbootquestions.entity.QuestionAnswers;
 import com.hendisantika.springbootquestions.service.QuestionBankService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -97,4 +104,25 @@ public class QuestionBankUIController {
         questionBankService.deleteQuestion(questionId);
         return "redirect:/questions";
     }
+
+    @GetMapping("/oauthinfo")
+    @ResponseBody
+    public String oauthUserInfo(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                                @AuthenticationPrincipal OAuth2User oauth2User) {
+        return
+                "User Name: " + oauth2User.getName() + "<br/>" +
+                        "User Authorities: " + oauth2User.getAuthorities() + "<br/>" +
+                        "Client Name: " + authorizedClient.getClientRegistration().getClientName() + "<br/>" +
+                        this.prettyPrintAttributes(oauth2User.getAttributes());
+    }
+
+    private String prettyPrintAttributes(Map<String, Object> attributes) {
+        String acc = "User Attributes: <br/><div style='padding-left:20px'>";
+        for (String key : attributes.keySet()) {
+            Object value = attributes.get(key);
+            acc += "<div>" + key + ":&nbsp" + value.toString() + "</div>";
+        }
+        return acc + "</div>";
+    }
+
 }
