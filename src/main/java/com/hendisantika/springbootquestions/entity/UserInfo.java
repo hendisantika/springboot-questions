942b08c98695ebb12a4b85905cde2ef90e2e4e4c
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:25:21 +0700

    #6 - Add UserInfo Entity

---
+++ src/main/java/com/hendisantika/springbootquestions/entity/UserInfo.java
@@ -0,0 +1,47 @@
+package com.hendisantika.springbootquestions.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-questions
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.24
+ */
+@Entity
+@Table(name = "user_info")
+@NoArgsConstructor
+public class UserInfo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(insertable = false, name = "user_id", nullable = false)
+    private Integer userId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "pwd", nullable = false)
+    private String pwd;
+
+    @Column(name = "roles")
+    private String roles;
+
+    UserInfo(String username, String pwd, String roles) {
+        this.username = username;
+        this.pwd = pwd;
+        this.roles = roles;
+    }
+}
