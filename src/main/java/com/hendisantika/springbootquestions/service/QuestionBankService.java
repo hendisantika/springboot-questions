5a471b61db4d93d7f056c0f7f48f3d82c74e4f03
Hendi Santika <hendisantika@yahoo.co.id>
2020-11-11 08:32:39 +0700

    #12 - Add QuestionBankService

---
+++ src/main/java/com/hendisantika/springbootquestions/service/QuestionBankService.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootquestions.service;
+
+import com.hendisantika.springbootquestions.repository.CategoryRepository;
+import com.hendisantika.springbootquestions.repository.QuestionAnswersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-questions
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/11/20
+ * Time: 08.32
+ */
+@Service
+public class QuestionBankService implements IQuestionBankService {
+
+    @Autowired
+    private QuestionAnswersRepository questionAnswersRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+}
